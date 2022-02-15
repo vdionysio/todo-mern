f435374ecf30abcdd0aee90ad7d4ca2a6ac0f24a
vdionysio <vini.dionysio@gmail.com>
2022-02-15 19:49:18 -0300

    fix(server): change port fom 500 to 5000

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const routes = require('./src/routes');
 const middlewares = require('./src/middlewares');
 
-const PORT = process.env.PORT || 500;
+const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
 
