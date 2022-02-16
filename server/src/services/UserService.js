906bd3648a283df5a82b6981f063b961a5e54361
vdionysio <vini.dionysio@gmail.com>
2022-02-16 18:32:45 -0300

    feat(server): create login function on user service

--- server/src/services/UserService.js
+++ server/src/services/UserService.js
@@ -1,5 +1,6 @@
 const User = require('../models/User');
 const UserSchema = require('../schemas/userSchema');
+const loginSchema = require('../schemas/loginSchema');
 const { validateError } = require('../helpers');
 
 const create = async (input) => {
@@ -17,6 +18,22 @@ const create = async (input) => {
   }
 };
 
+const login = async (credentials) => {
+  const { error } = loginSchema.validate(credentials);
+
+  if (error) throw validateError(400, error.message);
+
+  const userExists = await User.findOne({
+    email: credentials.email,
+    password: credentials.password,
+  });
+
+  if (!userExists) throw validateError(400, 'Invalid fields');
+
+  return true;
+};
+
 module.exports = {
   create,
+  login,
 };
