339107e230c0fe36d3653241bdec17c7291f00e9
vdionysio <vini.dionysio@gmail.com>
2022-02-16 07:20:51 -0300

    chore(client): add eslint + prettier config

--- client/src/components/TestComponent.js
+++ client/src/components/TestComponent.js
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function TestComponent() {
   const [result, setResult] = useState();
   const [registerInputs, setRegisterInputs] = useState({
     displayName: 'testando',
-    email: 'teste2asdasd21@gmail.com',
+    email: 'teste2asdasaad21@gmail.com',
     password: '12asdasd78',
   });
 
