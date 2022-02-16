f2c909f4b516e8e78b8548b19ba425c637d7e985
vdionysio <vini.dionysio@gmail.com>
2022-02-16 18:18:02 -0300

    feat(server): add validate JWT middleware

---
+++ server/src/middlewares/validateJWT.js
@@ -0,0 +1,31 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+module.exports = async (req, res, next) => {
+  const token = req.headers.authorization;
+  const JWT_SECRET = process.env.JWT_SECRET || 'secretkey';
+  if (!token) {
+    const err = new Error('missing auth token');
+    err.statusCode = 401;
+
+    return next(err);
+  }
+
+  try {
+    const payload = jwt.verify(token, JWT_SECRET);
+    const user = await User.findOne({ email: payload.email });
+    console.log(payload);
+
+    if (!user) {
+      const err = new Error('User not registered');
+      err.statusCode = 401;
+      return next(err);
+    }
+    req.user = payload;
+    return next();
+  } catch (err) {
+    err.message = 'jwt malformed';
+    err.statusCode = 401;
+    return next(err);
+  }
+};
