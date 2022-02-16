281232f8b4bdf67ccd037da6da426d60727dac9b
vdionysio <vini.dionysio@gmail.com>
2022-02-16 18:17:19 -0300

    test(server): add validateJWT middleware test

---
+++ server/__tests__/unit/middlewares/validateJWT.test.js
@@ -0,0 +1,82 @@
+const { getMockReq, getMockRes } = require('@jest-mock/express');
+const jwt = require('jsonwebtoken');
+const { generateToken } = require('../../../src/helpers');
+const { validateJWT } = require('../../../src/middlewares');
+const User = require('../../../src/models/User');
+const db = require('../../db');
+
+describe('validateJWT middleware', () => {
+  let savedUser;
+  beforeAll(async () => {
+    await db.setUp();
+    const newUser = new User({
+      displayName: 'Dionysio',
+      email: 'dionysio@gmail.com',
+      password: '123456789',
+    });
+    savedUser = await newUser.save();
+  });
+
+  afterEach(async () => {
+    await db.dropCollections();
+    jest.restoreAllMocks();
+  });
+
+  afterAll(async () => {
+    await db.dropDatabase();
+  });
+
+  it('When the token is valid should call next and load user on req', async () => {
+    const token = generateToken(savedUser.email);
+    const req = getMockReq({
+      headers: {
+        authorization: token,
+      },
+    });
+    const { res, next } = getMockRes();
+
+    await validateJWT(req, res, next);
+
+    expect(next).toHaveBeenCalledWith();
+    expect(req.user).toMatchObject(
+      jwt.verify(token, process.env.JWT_SECRET || 'secretkey')
+    );
+  });
+
+  it('When the token is missing should call next with an error message "missing auth token"', async () => {
+    const req = getMockReq();
+    const { res, next } = getMockRes();
+
+    await validateJWT(req, res, next);
+
+    expect(next).toHaveBeenCalledWith(Error('missing auth token'));
+    // await expect(validateJWT(req, res, next)).resolves.toThrowError('I should fail');
+  });
+
+  it('When the token is invalid should call next with an error message "jwt malformed"', async () => {
+    const req = getMockReq({
+      headers: {
+        authorization: 'invalidToken',
+      },
+    });
+    const { res, next } = getMockRes();
+
+    await validateJWT(req, res, next);
+
+    expect(next).toHaveBeenCalledWith(Error('jwt malformed'));
+  });
+
+  it('When the token is not associated with a valid user should call next with error message "User not registered"', async () => {
+    const token = generateToken('notregistered@gmail.com');
+    const req = getMockReq({
+      headers: {
+        authorization: token,
+      },
+    });
+    const { res, next } = getMockRes();
+
+    await validateJWT(req, res, next);
+
+    expect(next).toHaveBeenCalledWith(Error('User not registered'));
+  });
+});
--- server/src/middlewares/index.js
+++ server/src/middlewares/index.js
@@ -1,5 +1,7 @@
 const error = require('./error');
+const validateJWT = require('./validateJWT');
 
 module.exports = {
   error,
+  validateJWT,
 };
