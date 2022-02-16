eb048b0f8d7662ecc35224532e3d398a8afaa177
vdionysio <vini.dionysio@gmail.com>
2022-02-16 16:13:19 -0300

    feat(server): create task model

---
+++ server/src/models/Task.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const TaskSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      index: { unique: true },
+    },
+    description: {
+      type: String,
+      required: false,
+    },
+    status: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model('Task', TaskSchema);
